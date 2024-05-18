3b2d0730a79e600b8a237dfcb9a6da9be399f07f
Naveen Kumar Peddi <peddin5739@gmail.com>
2024-05-18 17:32:38 -0500

    Update index.js

--- AutoApi/index.js
+++ AutoApi/index.js
@@ -1,7 +1,7 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-
+// This is the base path
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from Auto Api!",
