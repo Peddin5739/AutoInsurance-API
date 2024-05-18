5a94e07f45831696bf47c0b8a1ae7f0d93375dc8
Naveen Kumar Peddi <peddin5739@gmail.com>
2024-05-18 17:25:58 -0500

    added validate user endpoint

--- AutoApi/index.js
+++ AutoApi/index.js
@@ -14,6 +14,12 @@ app.get("/path", (req, res, next) => {
   });
 });
 
+app.get("/validateUser", (req, res, next) => {
+  return res.status(200).json({
+    message: "user ok",
+  });
+});
+
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
